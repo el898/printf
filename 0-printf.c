9675172819486fa5b471c627f529095119dcb272
xhepan <labs6068@gmail.com>
2023-01-22 03:41:24 -0800

    task 0

---
+++ 0-printf.c
@@ -0,0 +1,16 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * _printf - implements standard printf
+ * @format - string to write on screen
+ *
+ * Return: number of charecters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	
+}
