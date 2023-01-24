2cdc9e0fa80dd8a8bd5368d1ba8f847e80fa8245
el898 <ebubeexcel3@gmail.com>
2023-01-24 14:00:08 -0800

    printf

--- _printf.c
+++ _printf.c
@@ -1,16 +1,12 @@
 #include "main.h"
-
 void print_buffer(char buffer[], int *buff_ind);
-
 /**
  * _printf - Printf function
  * @format: format.
  * Return: Printed chars.
  */
 int _printf(const char *format, ...)
-
 {
-
 	int i, printed = 0, printed_chars = 0;
 
 	int flags, width, precision, size, buff_ind = 0;
@@ -103,7 +99,6 @@ int _printf(const char *format, ...)
  * @buff_ind: Index at which to add next char, represents the length.
  */
 void print_buffer(char buffer[], int *buff_ind)
-
 {
 
 	if (*buff_ind > 0)
